4a826be9dd7635da3e01b20e6ad09a528b9467f4
Louis Dupraz <louis.dupraz@epitech.eu>
2023-04-12 14:42:05 +0200

    Cleaned echo and added 2 / 3 missing tests

--- built-in/echo.c
+++ built-in/echo.c
@@ -9,17 +9,19 @@
 
 int echo(command_t *command)
 {
-    int nquotes = 0;
-    int i = 5;
+    int nquotes = 0, i = 5;
     if (strlen(command->command) <= 5) {
-        write(1, "\n", 1); return 0;
+        write(1, "\n", 1);
+        return 0;
     }
     while (command->command[i]) {
         if (command->command[i] == '"' || command->command[i] == '\'') {
-            nquotes++; i++;
+            nquotes++;
+            i++;
         }
         while (command->command[i] == ' ' && command->command[i + 1] == ' '
-        && (nquotes % 2 == 0)) i++;
+        && (nquotes % 2 == 0))
+            i++;
         write(1, &command->command[i], 1);
         i++;
     }
