7238c37cc270054ebd0a4744206588a9cf5a2fd2
Valentin <valentin.rapp@epitech.eu>
2023-05-02 11:29:16 +0200

    feat: struct for linked_lists (will be useful for implementing aliases later on)

---
+++ lib/linked_lists/linked_lists.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** header
+** File description:
+** header
+*/
+
+#ifndef header
+    #define header
+
+// data_a -> string to replace
+// data_b -> what to replace it with
+
+typedef struct node
+{
+    char *data_a;
+    char *data_b;
+    struct node *next;
+} node_t;
+
+#endif /* !header */
