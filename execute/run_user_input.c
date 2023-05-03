fd8808906bea0dbbdac1190fbe8696d96c2c2d91
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-03 22:53:50 +0200

    fix: memleak

--- execute/run_user_input.c
+++ execute/run_user_input.c
@@ -31,24 +31,35 @@ int run_command(command_t *command, int *exiting, envdata_t *env)
     return (status);
 }
 
-int run_user_input(char *input, envdata_t *env, int *exiting)
+int execution_loop(command_t **commands, int nb_commands, int *exiting,
+    envdata_t *env)
 {
-    char *res = strdup(input);
-    if (process_quotes(&res)) return -1;
-    command_t **commands = cut_input_to_commands(res);
-    int status = 0, nb_commands = 0, i = 0;
-    if (commands == NULL) return status;
-    for (; commands[nb_commands] != NULL; nb_commands++);
+    int status = 0, i = 0;
     int *data[3] = {&i, &nb_commands, exiting};
     for (; i < nb_commands && status != -1; i++) {
         if (i > 0 && ((commands[i - 1]->condition == AND && status) ||
-            (commands[i - 1]->condition == OR && !status))) continue;
+            (commands[i - 1]->condition == OR && !status))) {
+            free_command(commands[i]);
+            continue;
+        }
         status = 0;
         if (!commands[i]->pipe_out)
             status = run_command(commands[i], exiting, env);
         else
             status = loop_over_pipes(commands, env, data);
     }
+    return status;
+}
+
+int run_user_input(char *input, envdata_t *env, int *exiting)
+{
+    char *res = strdup(input);
+    if (process_quotes(&res)) return -1;
+    command_t **commands = cut_input_to_commands(res);
+    int nb_commands = 0;
+    if (commands == NULL) return 0;
+    for (; commands[nb_commands] != NULL; nb_commands++);
+    int status = execution_loop(commands, nb_commands, exiting, env);
     free(commands);
     free(res);
     return (status);
