d4627285e55457c1b9b5158589cd34c49f7674ad
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-03 21:06:55 +0200

    fix: coding style

--- execute/run_user_input.c
+++ execute/run_user_input.c
@@ -34,18 +34,15 @@ int run_command(command_t *command, int *exiting, envdata_t *env)
 int run_user_input(char *input, envdata_t *env, int *exiting)
 {
     char *res = strdup(input);
-    if (process_quotes(&res))
-        return -1;
+    if (process_quotes(&res)) return -1;
     command_t **commands = cut_input_to_commands(res);
     int status = 0, nb_commands = 0, i = 0;
-    if (commands == NULL)
-        return status;
+    if (commands == NULL) return status;
     for (; commands[nb_commands] != NULL; nb_commands++);
     int *data[3] = {&i, &nb_commands, exiting};
     for (; i < nb_commands && status != -1; i++) {
         if (i > 0 && ((commands[i - 1]->condition == AND && status) ||
-            (commands[i - 1]->condition == OR && !status)))
-            continue;
+            (commands[i - 1]->condition == OR && !status))) continue;
         status = 0;
         if (!commands[i]->pipe_out)
             status = run_command(commands[i], exiting, env);
