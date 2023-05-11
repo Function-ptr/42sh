8531b31001e8d67d2edc72301f8a6eeb140a6e88
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-11 15:20:33 +0200

    feat: started replacing variables

--- parse/replace_variables.c
+++ parse/replace_variables.c
@@ -15,8 +15,48 @@
                              |___/               |______|
 */
 #include "parsing.h"
+#include "environment.h"
 
+char *get_variable_name_in_command(char *dollar)
+{
+    char *next = strchr(dollar, ' ');
+    if (!next)
+        return strdup(dollar + 1);
+    char *name = strndup(dollar + 1, next - dollar);
+    name[next - dollar - 1] = 0;
+    return name;
+}
 
+char *replace_variable_with_value(char *dollar, char *value, char *varname,
+    char *command)
+{
+    size_t valuelen = strlen(value), namelen = strlen(varname);
+    size_t lenend = strlen(dollar), offset = valuelen - namelen;
+    size_t len = strlen(command);
+    char *backup = strdup(dollar);
+    char *new = reallocarray(command, len + offset, sizeof(char));
+
+}
+
+void replace_variables(char **cmd, envdata_t *env)
+{
+    char *command = *cmd;
+    char *dollar = strchr(command, '$');
+    while (dollar) {
+        size_t pos = dollar - command;
+        char *varname = get_variable_name_in_command(dollar);
+        char *var = get_environment_variable(env->env, varname);
+        if (var == NULL)
+            var = get_var_value(env->variables, varname);
+        if (var != NULL) {
+            *cmd = replace_variable_with_value(dollar, var, varname, command);
+            command = *cmd;
+            dollar = command + pos;
+        }
+        free(varname);
+        dollar = strchr(dollar + 1, '$');
+    }
+}
 /*
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠉⠉⢉⠏⠻⣍⠑⢲⠢⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⣻⣿⢟⣽⠿⠯⠛⡸⢹⠀⢹⠒⣊⡡⠜⠓⠢⣄⠀⠀⠀⠀
