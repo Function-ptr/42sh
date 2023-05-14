34e3943d842db0fdbe4307d464840bca708d5ca8
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-14 17:54:51 +0200

    feat: added -n flag

--- built-in/echo.c
+++ built-in/echo.c
@@ -7,12 +7,34 @@
 
 #include "built_in.h"
 #include "prompt.h"
+#include "my.h"
+#include "parsing.h"
+
+int has_n_flag(char **command)
+{
+    char **args = my_str_to_word_array(*command, " \t");
+    int alen = my_char_arraylen(args), has_flag = 0, s = 0;
+    for (int i = 1; i < alen; i++) {
+        int arglen = (int)strlen(args[i]);
+        if (arglen != 2 || args[i][0] != '-') break;
+        if (!strcmp(args[i], "-n")) has_flag = 1;
+    }
+    for (int i = 0; i < alen; i++)
+        free(args[i]);
+    free(args);
+    if (has_flag) {
+        char *flagpos = strstr(*command, "-n");
+        memset(flagpos, ' ', 2);
+        *command = remove_spaces_in_command(*command, 0, false, &s);
+    }
+    return has_flag;
+}
 
 int echo(command_t *command)
 {
-    int nquotes = 0, i = 5;
+    int nquotes = 0, i = 5, nonew = has_n_flag(&(command->command));
     if (strlen(command->command) <= 5) {
-        dprintf(command->out_fd, "\n");
+        if (!nonew) dprintf(command->out_fd, "\n");
         return 0;
     }
     while (command->command[i]) {
@@ -21,12 +43,11 @@ int echo(command_t *command)
             i++;
         }
         while (command->command[i] == ' ' && command->command[i + 1] == ' '
-        && (nquotes % 2 == 0))
-            i++;
+        && (nquotes % 2 == 0)) i++;
         dprintf(command->out_fd, "%c", command->command[i]);
         i++;
     }
-    dprintf(command->out_fd, "\n");
+    if (!nonew) dprintf(command->out_fd, "\n");
     return 0;
 }
 
